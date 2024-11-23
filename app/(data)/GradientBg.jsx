0fceff93691214f82b0a80f8bb4851d351c0ed9a
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-23 16:42:51 +0530

    removed extra colors

--- app/(data)/GradientBg.jsx
+++ app/(data)/GradientBg.jsx
@@ -10,37 +10,5 @@ export default [
     {
         "name": "Sunset",
         "gradient": "linear-gradient(to right, #2e3192, #1bffff)"
-    },
-    {
-        "name": "Forest",
-        "gradient": "linear-gradient(to right, #ff5e3a, #f0ca4c)"
-    },
-    {
-        "name": "Ocean",
-        "gradient": "linear-gradient(to right, #43cea2, #185a93)"
-    },
-    {
-        "name": "Sky",
-        "gradient": "linear-gradient(to right, #24cfe8, #7368aa)"
-    },
-    {
-        "name": "Fire",
-        "gradient": "linear-gradient(to right, #ca4337, #f09849)"
-    },
-    {
-        "name": "Night",
-        "gradient": "linear-gradient(to right, #222222, #000000)"
-    },
-    {
-        "name": "Neon",
-        "gradient": "linear-gradient(to right, #00f2ff, #f7cac9)"
-    },
-    {
-        "name": "Mint",
-        "gradient": "linear-gradient(to right, #9dff95, #cfdc83)"
-    },
-    {
-        "name": "Grapefruit",
-        "gradient": "linear-gradient(to right, #ffc0cb, #f7cac9)"
-    },
+    }
 ];
