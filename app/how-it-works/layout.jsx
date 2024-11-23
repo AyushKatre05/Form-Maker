a858389e29d6092de4b814146a61d7bbb31b080f
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-23 16:46:40 +0530

    no sidebar

--- app/how-it-works/layout.jsx
+++ app/how-it-works/layout.jsx
@@ -2,19 +2,13 @@
 
 import { SignedIn } from '@clerk/nextjs';
 import React from 'react';
-import SideNav from '../dashboard/(components)/SideNav';
 
 const Layout = ({ children }) => {
   return (
     <SignedIn>
       <div className="flex flex-col md:flex-row">
-        <div className="md:w-64 md:fixed md:h-full">
-          <SideNav />
-        </div>
-        <div className="flex-1 md:ml-64 p-3">
           {children}
         </div>
-      </div>
     </SignedIn>
   );
 };
