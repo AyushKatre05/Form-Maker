2032fb89a162c1c6099ba5703aad604ee1fe48d1
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-23 14:25:17 +0530

    langing page changes

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Form Builder Using AI",
+  title: "Colossus",
   description: "Made By Ayush",
 };
 
