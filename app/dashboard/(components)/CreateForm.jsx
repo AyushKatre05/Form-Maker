aa72f55f07854a49ddd4cb2a0573f96134cbb0c6
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-23 16:44:24 +0530

    remove unused params

--- app/dashboard/(components)/CreateForm.jsx
+++ app/dashboard/(components)/CreateForm.jsx
@@ -6,7 +6,6 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
